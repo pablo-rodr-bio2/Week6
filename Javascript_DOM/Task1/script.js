aa4caf3e8001c3f0e29cdef5b5610ba19251265e
pablo-rodr-bio2 <pablo.rodriguez@toptal.com>
2022-11-30 09:48:47 +0100

    DOM Task1 cleaned up and make it functional

--- Javascript_DOM/Task1/script.js
+++ Javascript_DOM/Task1/script.js
@@ -1,4 +1,10 @@
 class CountDown {
+  
+    cancel(e, id) {
+      document.getElementById(id).parentElement.remove()
+    }
+
+    
     start() {
         let userCountdown = document.getElementById('countdown').value
         const [hours, minutes, seconds] =  userCountdown.split(':')
@@ -19,19 +25,22 @@ class CountDown {
     
         // adding the cancel Button
         let cancelBtn = document.createElement('button')
-        cancelBtn.setAttribute('id', `cancel-${clicks}`)
+        let id = `cancel-${clicks}`
+        cancelBtn.setAttribute('id', id)
         cancelBtn.innerHTML = 'Cancel'
-        cancelBtn.addEventListener('click', cancel, false)
+        let algo = new CountDown()
+        cancelBtn.addEventListener('click', function(e) {algo.cancel(e, id)}, false)
         newCountDown.appendChild(cancelBtn)
     
-        newCountDown.getAttribute('id')
+       
     
         document.getElementById('results').appendChild(newCountDown)
     
         
-    
+        
         let x = setInterval(function() {
-    
+
+
             // Get today's date and time
             let now = new Date().getTime();
           
@@ -54,75 +63,15 @@ class CountDown {
           }, 1000);
     }
 
-    end() {
-
-    }
 }
 
 
 let clicks = 0
 let newCountDownObject = new CountDown()
 
-document.getElementById('myBtn').addEventListener('click', newCountDownObject.start)
-function start() {
-    // get input form user
-    [hours, minutes, seconds] = document.getElementById('countdown').value.split(':')
+document.getElementById('myBtn').addEventListener('click', function() {newCountDownObject.start()}, false)
 
-    // counter used for div's id
-    clicks += 1
 
-    //create new counter
-    let newCountDown = document.createElement('div')
-    newCountDown.setAttribute('id', `countDown-${clicks}`)
-
-    let countDownDate = new Date()
-    countDownDate.setHours(hours, minutes, seconds)
-
-    let timer = document.createElement('span')
-    timer.setAttribute('id', `timer-${clicks}`)
-    newCountDown.appendChild(timer)
-
-    // adding the cancel Button
-    let cancelBtn = document.createElement('button')
-    cancelBtn.setAttribute('id', `cancel-${clicks}`)
-    cancelBtn.innerHTML = 'Cancel'
-    cancelBtn.addEventListener('click', cancel, false)
-    newCountDown.appendChild(cancelBtn)
-
-    newCountDown.getAttribute('id')
-
-    document.getElementById('results').appendChild(newCountDown)
-
-    
-
-    let x = setInterval(function() {
-
-        // Get today's date and time
-        let now = new Date().getTime();
-      
-        // Find the distance between now and the count down date
-        let distance = countDownDate - now;
-      
-        // Time calculations for days, hours, minutes and seconds
-        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      
-        // Display the result in the element with id="demo"
-        timer.textContent =  hours + "h " + minutes + "m " + seconds + "s ";
-      
-        // If the count down is finished, write some text
-        if (distance < 0) {
-          clearInterval(x);
-          timer.textContent = "EXPIRED";
-        }
-      }, 1000);
-
-}
-
-function cancel(e) {
-    this.parentElement.remove();
-}
 
 
 
